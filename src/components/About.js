1e5ffe8550eb7a6d110d9d49e58d96e9cd472899
Drake Nguyen <drakeknguyen@yahoo.com>
2020-12-12 20:39:19 -0800

    add profile picture

--- src/components/About.js
+++ src/components/About.js
@@ -3,24 +3,30 @@ import React from "react";
 const About = (props) => {
   if (props.data) {
     var { paragraph1, paragraph2, paragraph3, paragraph4 } = props.data.bio;
+    var { resumelink } = props.data;
   }
 
   return (
     <section id="About" className="js--sections-about">
       <h2>About Me</h2>
-      <div className="js--wp-about">
-        <p>{paragraph1}</p>
-        <p>{paragraph2}</p>
-        <p>{paragraph3}</p>
-        <p>{paragraph4}</p>
-      <a
-        className="download_btn"
-        target="_blank"
-        rel="noreferrer"
-        href="https://drakenguyen4000.github.io/portfolio/7-download/DrakeNguyen-Resume.pdf"
-      >
-        <i className="fas fa-cloud-download-alt"></i> Download Resume
-      </a>
+      <div className="about-container">
+        <div className="js--wp-about">
+          <p>{paragraph1}</p>
+          <p>{paragraph2}</p>
+          <p>{paragraph3}</p>
+          <p>{paragraph4}</p>
+        </div>
+        <div className="about-container-2">
+          <img className="profile-pic" />
+          <a
+            className="download_btn"
+            target="_blank"
+            rel="noreferrer"
+            href={`${resumelink}`}
+          >
+            <i className="fas fa-cloud-download-alt"></i> Download Resume
+          </a>
+        </div>
       </div>
     </section>
   );

1401aed7c19cc34108eaf22d6ff904369c7daa2e
Drake Nguyen <drakeknguyen@yahoo.com>
2023-08-16 20:51:19 -0700

    revised to mobile first responsiveness

--- src/components/About.js
+++ src/components/About.js
@@ -2,17 +2,18 @@ import React from "react";
 
 const About = (props) => {
   if (props.data) {
-    var { paragraph1, paragraph2, paragraph3, paragraph4, paragraph5 } = props.data.bio;
+    var { paragraph1, paragraph2, paragraph3, paragraph4, paragraph5 } =
+      props.data.bio;
   }
 
   return (
     <section id="About" className="js--sections-about">
       <div className="about-container">
-      <div className="js--wp-about-pic about-container-pic">
+        <div className="js--wp-about-pic about-container-pic">
           <img className="profile-pic" />
         </div>
-        <div className="js--wp-about">
-        <h2>About Me</h2>
+        <div className="js--wp-about about-description">
+          <h2>About Me</h2>
           <p>{paragraph1}</p>
           <p>{paragraph2}</p>
           <p>{paragraph3}</p>

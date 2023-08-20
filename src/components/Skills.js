85ac0c35ce3003e2146d80646f25a8316954361f
Drake Nguyen <drakeknguyen@yahoo.com>
2023-08-19 19:56:42 -0700

    revised skills background and layout

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -19,29 +19,31 @@ const Skills = (props) => {
 
   return (
     <div className="skills-wrapper">
-    <section id="Skills" className="js--sections-skills">
-        <div className="skills-panels-group">
+      <section id="Skills" className="js--sections-skills">
+        <div>
           <h2>Web Development Skills</h2>
-          <span className="js--wp-skills-1 skills-panel panel-1">
-            <h3>
-              <span className="skills-title">Lang/Framework/Library</span>
-            </h3>
-            <ul>{skill(language)}</ul>
-          </span>
-          <span className="js--wp-skills-2 skills-panel panel-2">
-            <h3>
-              <span className="skills-title">Technical Skills</span>
-            </h3>
-            {<ul>{skill(skills)}</ul>}
-          </span>
-          <span className="js--wp-skills-3 skills-panel panel-3">
-            <h3>
-              <span className="skills-title">Tools</span>
-            </h3>
-            <ul>{skill(tools)}</ul>
-          </span>
+          <div className="skills-panels-group">
+            <span className="js--wp-skills-1 skills-panel panel-1">
+              <h3>
+                <span className="skills-title">Lang/Framework/Library</span>
+              </h3>
+              <ul>{skill(language)}</ul>
+            </span>
+            <span className="js--wp-skills-2 skills-panel panel-2">
+              <h3>
+                <span className="skills-title">Technical Skills</span>
+              </h3>
+              {<ul>{skill(skills)}</ul>}
+            </span>
+            <span className="js--wp-skills-3 skills-panel panel-3">
+              <h3>
+                <span className="skills-title">Tools</span>
+              </h3>
+              <ul>{skill(tools)}</ul>
+            </span>
+          </div>
         </div>
-    </section>
+      </section>
     </div>
   );
 };
